472cd5c13463e9d137a765424a07ffb41e1560ea
legoffchristophealexandre@gmail.com <legoffchristophealexandre@gmail.com>
2025-02-14 15:08:29 +0100

    update url

--- app/Views/template.php
+++ app/Views/template.php
@@ -17,16 +17,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Favicons pour différentes plateformes -->
-    <link rel="icon" type="image/png" href="<?= SITE_URL ?>assets/favicons/favicon-48x48.png" sizes="48x48" />
-    <link rel="icon" type="image/svg+xml" href="<?= SITE_URL ?>assets/favicons/favicon.svg" />
-    <link rel="shortcut icon" href="<?= SITE_URL ?>assets/favicons/favicon.ico" />
-    <link rel="apple-touch-icon" sizes="180x180" href="<?= SITE_URL ?>assets/favicons/apple-touch-icon.png" />
-    <link rel="manifest" href="<?= SITE_URL ?>assets/favicons/site.webmanifest" />
+    <link rel="icon" type="image/png" href="<?= SITE_URL ?>/assets/favicons/favicon-48x48.png" sizes="48x48" />
+    <link rel="icon" type="image/svg+xml" href="<?= SITE_URL ?>/assets/favicons/favicon.svg" />
+    <link rel="shortcut icon" href="<?= SITE_URL ?>/assets/favicons/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= SITE_URL ?>/assets/favicons/apple-touch-icon.png" />
+    <link rel="manifest" href="<?= SITE_URL ?>/assets/favicons/site.webmanifest" />
     <!-- Feuilles de style -->
     <link rel="stylesheet" href="https://bootswatch.com/5/superhero/bootstrap.min.css">
-    <link rel="stylesheet" href="<?= SITE_URL ?>assets/css/styles.css">
+    <link rel="stylesheet" href="<?= SITE_URL ?>/assets/css/styles.css">
     <!-- Script JavaScript principal -->
-    <script src="<?= SITE_URL ?>assets/js/main.js" defer></script>
+    <script src="<?= SITE_URL ?>/assets/js/main.js" defer></script>
     <title>Biblio | <?= $titre; ?></title>
 </head>
 
